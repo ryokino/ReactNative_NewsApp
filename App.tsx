d606b0bcf9db055fafc0edbc1309c99d3151ba09
ryokino <ryo.kinoshita212@gmail.com>
2022-01-12 14:24:30 +0900

    Sectio3 Done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.itemContainer}>
+                <View style={styles.itemLeftContainer}>
+                    <Image
+                        style={{ width: 100, height: 100 }}
+                        source={{
+                            uri: 'https://picsum.photos/200/300',
+                        }}
+                    />
+                </View>
+                <View style={styles.itemRightContainer}>
+                    <Text numberOfLines={3} style={styles.content}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </Text>
+                    <Text style={styles.source}>React News</Text>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        height: 100,
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    itemLeftContainer: {
+        width: 100,
+    },
+    itemRightContainer: {
+        flex: 2,
+        height: 100,
+        padding: 10,
+        justifyContent: 'space-between',
+    },
+    content: {
+        fontSize: 16,
+    },
+    source: {
+        fontSize: 12,
+        color: 'rgba(0,0,0,0.5)',
+    },
 });
